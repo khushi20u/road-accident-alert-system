8257de2c9334794f374e9dc12e1fba1d504972ea
khushi20u <khushisag2@gmail.com>
2026-06-26 13:59:29 +0530

    changes

--- backend/main.py
+++ backend/main.py
@@ -13,7 +13,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://localhost:3000", "https://road-accident-alert-system.vercel.app/"],
+    allow_origins=["http://localhost:5173", "http://localhost:3000", "https://road-accident-alert-system.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
